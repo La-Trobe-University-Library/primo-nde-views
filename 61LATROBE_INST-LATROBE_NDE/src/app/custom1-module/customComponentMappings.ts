4c5ca8fb149740a0409be10309ed584fe333b4c7
ShannonH-LTU <S.Hastwell@latrobe.edu.au>
2026-03-19 21:14:07 +1100

    Added LibChat
    Added the LibChat widget along with some styling for it.

--- 61LATROBE_INST-LATROBE_NDE/src/app/custom1-module/customComponentMappings.ts
+++ 61LATROBE_INST-LATROBE_NDE/src/app/custom1-module/customComponentMappings.ts
@@ -1,6 +1,8 @@
+import { LibchatComponent } from "../libchat/libchat.component";
+
 // Define the map
 export const selectorComponentMap = new Map<string, any>([
 
-    
+    ['nde-footer-before', LibchatComponent]
 
 ]);
