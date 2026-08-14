273b641b2b8f6cd335eeb2822333e53adeb86c2f
ShannonH-LTU <S.Hastwell@latrobe.edu.au>
2026-08-14 10:03:30 +1000

    Added the TRENDALL_COLL view for the Trendall collection.

---
+++ 61LATROBE_INST-TRENDALL_COLL/src/app/browse-home-page-content/browse-home-page-content.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'custom-browse-home-page-content',
+  standalone: true,
+  imports: [TranslateModule],
+  templateUrl: './browse-home-page-content.component.html',
+  styleUrl: './browse-home-page-content.component.scss'
+})
+export class BrowseHomePageContentComponent {
+
+}
