ac8e612e35b79f3fe58007037501d2df310cd9ce
ShannonH-LTU <S.Hastwell@latrobe.edu.au>
2026-09-03 14:08:47 +1000

    Removed component that hid password change
    The CATONLY and TREND_COLL views have been updated so they no longer use the custom component that hid the password change option for SSO user groups (as Primo NDE now supports that feature).

--- 61LATROBE_INST-CATONLY/src/app/custom1-module/customComponentMappings.ts
+++ 61LATROBE_INST-CATONLY/src/app/custom1-module/customComponentMappings.ts
@@ -1,7 +1,7 @@
 import { AvailabilityCountComponent } from "../availability-count/availability-count.component";
 import { BrowseHomePageContentComponent } from "../browse-home-page-content/browse-home-page-content.component";
 import { CollectionAuthorDateComponent } from "../collection-author-date/collection-author-date.component";
-import { CredentialChangesComponent } from "../credential-changes/credential-changes.component";
+//import { CredentialChangesComponent } from "../credential-changes/credential-changes.component";
 import { DatabaseHomepageNoticeComponent } from "../database-homepage-notice/database-homepage-notice.component";
 import { PrefilledAdvancedSearchComponent } from "../prefilled-advanced-search/prefilled-advanced-search.component";
 import { RequestStatusComponent } from "../request-status/request-status.component";
@@ -14,7 +14,7 @@ export const selectorComponentMap = new Map<string, any>([
     ['nde-search-filters-side-nav-bottom', AvailabilityCountComponent],
     ['nde-database-home-page-bottom', DatabaseHomepageNoticeComponent],
     ['nde-browse-home-page-bottom', BrowseHomePageContentComponent],
-    ['nde-personal-settings-bottom', CredentialChangesComponent],
+    //['nde-personal-settings-bottom', CredentialChangesComponent],
     ['nde-account-section-results-bottom', RequestStatusComponent],
     ['nde-requests-overview-bottom', RequestStatusComponent],
     ['nde-advanced-search-bottom', PrefilledAdvancedSearchComponent],
--- 61LATROBE_INST-TRENDALL_COLL/src/app/custom1-module/customComponentMappings.ts
+++ 61LATROBE_INST-TRENDALL_COLL/src/app/custom1-module/customComponentMappings.ts
@@ -1,7 +1,7 @@
 import { AvailabilityCountComponent } from "../availability-count/availability-count.component";
 import { BrowseHomePageContentComponent } from "../browse-home-page-content/browse-home-page-content.component";
 import { CollectionAuthorDateComponent } from "../collection-author-date/collection-author-date.component";
-import { CredentialChangesComponent } from "../credential-changes/credential-changes.component";
+//import { CredentialChangesComponent } from "../credential-changes/credential-changes.component";
 import { DatabaseHomepageNoticeComponent } from "../database-homepage-notice/database-homepage-notice.component";
 import { GuidedTourComponent } from "../guided-tour/guided-tour.component";
 import { PrefilledAdvancedSearchComponent } from "../prefilled-advanced-search/prefilled-advanced-search.component";
@@ -17,7 +17,7 @@ export const selectorComponentMap = new Map<string, any>([
     ['nde-database-home-page-bottom', DatabaseHomepageNoticeComponent],
     ['nde-browse-home-page-bottom', BrowseHomePageContentComponent],
     ['nde-skip-links-after', GuidedTourComponent],
-    ['nde-personal-settings-bottom', CredentialChangesComponent],
+    //['nde-personal-settings-bottom', CredentialChangesComponent],
     ['nde-account-section-results-bottom', RequestStatusComponent],
     ['nde-requests-overview-bottom', RequestStatusComponent],
     ['nde-advanced-search-bottom', PrefilledAdvancedSearchComponent],
